bc055fe4e952478872a92f55fcdab93501deda5d
Christopher Schramm <cschramm@shakaweb.org>
2013-11-07 08:43:34 +0100

    Refactor runner

--- lib/rails-audit/runner.rb
+++ lib/rails-audit/runner.rb
@@ -1,58 +1,62 @@
 require 'yaml'
 
 module RailsAudit
-  def self.load_config
-    File.exist?('config/audit.yml') && YAML.load_file('config/audit.yml') || {}
-  end
-
-  def self.get_config(config, name)
-    c = config[name]
-    {
-      rails: config['Rails'].nil? && true || config['Rails'],
-      params: c && c['Parameters'] || '',
-      enabled: (c.nil? || c['Enabled'].nil?) && true || c['Enabled']
-    }
-  end
-
-  def self.run
-    config = load_config
-
-    concurrency = config['Concurrency'].nil? && true || config['Concurrency']
-
-    failures = concurrency && run_threads(config) || run_sequence(config)
-
-    if failures.any?
-      puts 'Failed tests:'
-      failures.each { |f| puts f }
-    end
-
-    failures.none?
-  end
-
-  def self.run_sequence(config)
-    failures = []
-
-    Audits::ALL.each do |audit|
-      success = audit.run get_config(config, audit.get_name)
-      failures << audit.get_name unless success
-    end
-
-    failures
-  end
-
-  def self.run_threads(config)
-    failures = []
-    mutex = Mutex.new
-
-    threads = Audits::ALL.map do |audit|
-      Thread.new do
-        success = audit.run get_config(config, audit.get_name)
-        mutex.synchronize { failures << audit.get_name unless success }
+  class Runner
+    def initialize
+      @config = { 'Concurrency' => true, 'Rails' => true }
+      if File.exist? 'config/audit.yml'
+        @config = @config.merge YAML.load_file('config/audit.yml')
       end
     end
 
-    threads.each { |t| t.join }
+    def get_config(name)
+      defaults = { 'Parameters' => '', 'Enabled' => true }
+      config = defaults.merge(@config[name] || {})
+      {
+        rails: @config['Rails'],
+        params: config['Parameters'],
+        enabled: config['Enabled']
+      }
+    end
 
-    failures
+    def run
+      concurrency = @config['Concurrency']
+
+      failures = concurrency && run_threads || run_sequence
+
+      if failures.any?
+        puts 'Failed tests:'
+        failures.each { |f| puts f }
+      end
+
+      failures.none?
+    end
+
+    def run_sequence
+      failures = []
+
+      Audits::ALL.each do |audit|
+        success = audit.run get_config(audit.get_name)
+        failures << audit.get_name unless success
+      end
+
+      failures
+    end
+
+    def run_threads
+      failures = []
+      mutex = Mutex.new
+
+      threads = Audits::ALL.map do |audit|
+        Thread.new do
+          success = audit.run get_config(audit.get_name)
+          mutex.synchronize { failures << audit.get_name unless success }
+        end
+      end
+
+      threads.each { |t| t.join }
+
+      failures
+    end
   end
 end
