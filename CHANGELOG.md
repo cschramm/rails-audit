f3f6b721c93d5cf69a17beb0d63f93848e5effff
Christopher Schramm <git@cschramm.eu>
2015-02-10 17:19:49 +0100

    Bump version to 0.35.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
 # Changelog
 
-## master
+## 0.35.0
 
 * New rubocop version 0.29
 
--- lib/rails-audit/version.rb
+++ lib/rails-audit/version.rb
@@ -1,7 +1,7 @@
 module RailsAudit
   class Version
     MAJOR = 0
-    MINOR = 34
+    MINOR = 35
     PATCH = 0
 
     def self.to_s
