f4da3bd081b9503f737b0240fc0aff54e08f8fdf
Christopher Schramm <git@cschramm.eu>
2015-08-06 07:28:38 +0200

    Update rubocop dependency to 0.33 and bump version to 0.39.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## 0.39.0
+
+* New rubocop version 0.33
+
 ## 0.38.0
 
 * New rubocop version 0.32
--- lib/rails-audit/version.rb
+++ lib/rails-audit/version.rb
@@ -1,7 +1,7 @@
 module RailsAudit
   class Version
     MAJOR = 0
-    MINOR = 38
+    MINOR = 39
     PATCH = 0
 
     def self.to_s
